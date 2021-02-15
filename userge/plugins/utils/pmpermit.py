9446d5f5db9a35e8dc5dd8e277fb3b10192c426c
MR-LEO <73919659+wonkru-bot@users.noreply.github.com>
2021-02-15 20:58:06 +0530

    🎶👀💃

--- userge/plugins/utils/pmpermit.py
+++ userge/plugins/utils/pmpermit.py
@@ -295,7 +295,7 @@ async def uninvitedPmHandler(message: Message):
         PMPERMIT_MSG[message.from_user.id] = (
             await message.reply(
                 noPmMessage.format_map(SafeDict(**user_dict))
-                + "\n`- Protected by USERGE-X`"
+                + "\n`- Protected by 🇱 🇪 🇴𝚉 ᴬˢˢᴵˢᵀᴬᴺᵀ`"
             )
         ).message_id
         await asyncio.sleep(1)
